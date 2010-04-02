9b627576fe02349e0a396eeca96229bc5cc8cda3
Karim Sumun <ksumun@gmail.com>
2010-04-02 12:06:13 +0100

    Changes from bzr repo

--- ex-1-18.scm
+++ ex-1-18.scm
@@ -6,39 +6,72 @@
 ;;; 2
 ;; <slist>             ::= '() | (<symbol-expression> . <slist>)
 ;; <symbol-expression> ::= <symbol> | <slist>
+
+;; Done!
+;; What was needed was an iterative function that returned either a
+;; success or error value. Since the success value is a function, the
+;; error value had to be wrapped as a constant function.
+
+(define symbol-or-compose
+  (lambda (e exp)
+    (if (null? exp)
+	e
+	(compose e exp))))
+
 (define car&cdr-h
-  (lambda (s slist errvalue rem ret)
+  (lambda (s slist success-val failure-val)
     (if (null? slist)
-	(if (null? rem)
-	    errvalue
-	    (compose ret cdr))
-;	    (car&cdr-h s rem errvalue rem (compose ret cdr)))
+	failure-val
 	(if (symbol? (car slist))
 	    (if (eqv? s (car slist))
-		(if (null? ret)
-		    car
-		    (compose ret car))
-		(car&cdr-h s (car slist) errvalue (cdr slist)
-			   (compose ret (car&cdr s (cdr slist) errvalue))))))))
+		(symbol-or-compose car success-val)
+		(car&cdr-h
+		 s (cdr slist) (symbol-or-compose cdr success-val) failure-val))
+	    (let ((res
+		   (car&cdr-h s (car slist) (symbol-or-compose car success-val)
+			      failure-val)))
+	      (if (not (eqv? res failure-val))
+		  res
+		  (car&cdr-h s (cdr slist) (symbol-or-compose cdr success-val)
+			     failure-val)))))))
 
 (define car&cdr
   (lambda (s slist errvalue)
-    (car&cdr-h s slist errvalue slist '())))
+    (let ((res (car&cdr-h s slist '() errvalue)))
+      (if (procedure? res)
+	  res
+	  (lambda (x) errvalue)))))
 
-(define car&cdr
-  (lambda (s slist errvalue)
+;;; 3
+;;; Like car&cdr-h but without using compose. Will probably have to return
+;;; lots of nested lambdas.
+
+(define symbol-or-lambda
+  (lambda (e exp)
+    (if (null? exp)
+	e
+	(lambda (x) (e (exp x))))))
+
+(define car&cdr-h2
+  (lambda (s slist success-val failure-val)
     (if (null? slist)
-	errvalue
-	(let ((first (car slist))
-	      (rest (cdr slist)))
-	  (if (symbol? first)
-	      (if (eqv? first s)
-		  car
-		  (compose (car&cdr s rest errvalue) cdr))
-	      (list compose (car&cdr s first errvalue) (car&cdr s rest errvalue)))))))
+	failure-val
+	(if (symbol? (car slist))
+	    (if (eqv? s (car slist))
+		(symbol-or-lambda car success-val)
+		(car&cdr-h2
+		 s (cdr slist) (symbol-or-lambda cdr success-val) failure-val))
+	    (let ((res
+		   (car&cdr-h2 s (car slist) (symbol-or-lambda car success-val)
+			       failure-val)))
+	      (if (not (eqv? res failure-val))
+		  res
+		  (car&cdr-h2 s (cdr slist) (symbol-or-lambda cdr success-val)
+			      failure-val)))))))
 
-> ((car&cdr 'a '((a b) c ((d e))) 'err) '((x y) z ((q r))))
-
-Error: attempt to call a non-procedure
-       ('(#{Procedure 8515 compose} #{Procedure 654 car} #{Procedure 8516 (unnamed in compose)}) '((x y) z ((q r))))
-
+(define car&cdr2
+  (lambda (s slist errvalue)
+    (let ((res (car&cdr-h2 s slist '() errvalue)))
+      (if (procedure? res)
+	  res
+	  (lambda (x) errvalue)))))
