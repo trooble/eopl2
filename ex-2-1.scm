ccea0018510defaea4612bafd2911b2c491bb849
Karim Sumun <ksumun@gmail.com>
2010-05-27 06:04:44 +0100

    Completed pred for ex 2.1

--- ex-2-1.scm
+++ ex-2-1.scm
@@ -17,23 +17,25 @@
 	      (cons 0 (b-succ (cdr n)))
 	      (cons b-succ-lsd (cdr n)))))))
 
+;;; x > 0
+;;; x y z -> (x - 1) y z
+;;; 0 y z -> base pred (y z)
 (define b-pred
   (lambda (n)
     (cond
      ((null? n) #f)
+     ((equal? n '(1)) '())
      ((zero? (car n))
       (if (null? (cdr n))
 	  #f
-	  (if (zero? (cadr n))
-	      (b-pred (cdr n))
-	      (cons (- base 1) (b-pred (cdr n))))))
+	  (cons (- base 1) (b-pred (cdr n)))))
      (else (cons (- (car n) 1) (cdr n))))))
 
 ; some numbers
 (define make-number
   (lambda (n)
     (if (zero? n)
-	zero
+	b-zero
 	(b-succ (make-number (- n 1))))))
 
 ; factorial 0 = 1
